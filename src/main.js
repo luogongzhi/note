e093c01a659af1a1d2f79faea52dd2a6e7991b97
l2468526234 <2041344657@qq.com>
2021-07-22 19:34:36 +0800

    update pwa

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 import store from './store';
-
 //通用字体
 import 'vfonts/Lato.css'
 //等宽字体
 import 'vfonts/FiraCode.css'
+import { registerSW } from 'virtual:pwa-register'
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // show a prompt to user
+  },
+  onOfflineReady() {
+    // show a ready to work offline to user
+  },
+})
+
 
 createApp(App).use(router).use(store).mount('#app')
